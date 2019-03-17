e5e0c6dd20005a7f13f321a327483d1828aa5d19
najak <kamilnaja@gmail.com>
2019-03-17 11:09:15 +0100

    added bq parser

--- markdownParser/RegExps.js
+++ markdownParser/RegExps.js
@@ -2,5 +2,8 @@ module.exports = Object.freeze({
     paragraphLine: /(.+)/g, //everything except non printable chars
     headerBlock: /^(<.*>)?.?(#{1,6})(.+)/g,
     emptyLine: /^(?![a-zA-Z0-9])/g,
-    blockQuote: /^(>)(.+)/g
+    blockQuote: /^(>)(.+)/g,
+    replaceBlockquoteTag: /(#)(.*)(<\/blockquote>)$/g,
+    startswithBlockquoteTag: /^<blockquote>/,
+    startsWithHash: /^#./g
 });
